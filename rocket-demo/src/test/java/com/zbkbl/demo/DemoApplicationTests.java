3b6155149288f7ca58df8645b5e175d77ced51d6
liuyang <liuyang04@kanzhun.com>
2020-04-14 15:53:21 +0800

    项目初始化

---
+++ rocket-demo/src/test/java/com/zbkbl/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbkbl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
